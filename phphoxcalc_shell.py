f6bfa64fed695f4a87808c7a9d88e2920aa1ee98
leybodv <leybodv@gmail.com>
2021-08-03 16:54:10 +0300

    added phphoxcalc_shell.py

---
+++ phphoxcalc_shell.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import cmd
+
+class PhPhOxCalcShell(cmd.Cmd):
+    intro = 'Process data of phenol photooxidation experiment. Type help or ? to list commands.\n'
+    prompt = '> '
+
+    def do_helloworld(self, arg):
+        """
+        Prints "Hello world" and all arguments passed here
+        """
+        print(f'Hello world: {arg}')
+
+    def do_quit(self):
+        """
+        Quits program
+        """
+        print('bye')
+        return True
+
+    if __name__ == '__main__':
+        PhPhOxCalcShell().cmdloop()

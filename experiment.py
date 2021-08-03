20050e5974fd24a18a7a1f766d7ad1cb284f529c
leybodv <leybodv@gmail.com>
2021-08-03 18:44:58 +0300

    new file:   experiment.py: added class representing experiment results; modified:   phphoxcalc_shell.py: added do_addexperiment method (to be continued)

---
+++ experiment.py
@@ -0,0 +1,5 @@
+class Experiment():
+
+    def __init__(self, sample_name, raw_data_path):
+        self.sample_name = sample_name
+        self.raw_data_path = raw_data_path
--- phphoxcalc_shell.py
+++ phphoxcalc_shell.py
@@ -5,12 +5,29 @@ import cmd
 class PhPhOxCalcShell(cmd.Cmd):
     intro = 'Process data of phenol photooxidation experiment. Type help or ? to list commands.\n'
     prompt = '> '
+    experiments = list() #experiment space
 
     def do_helloworld(self, arg):
         """
         Prints "Hello world" and all arguments passed here
         """
         print(f'Hello world: {arg}')
+
+    def do_addexperiment(self, arg):
+        """
+        Adds experiment results to the experiment's space.
+
+        Parameters:
+        -----------
+            arg : str
+                arguments provided by user in a form of key=value pairs separated by space. Allowed values:
+                    sample_name=name:
+                        Id of sample experiment were done with
+                    raw_data_path=path:
+                        Path to raw data file in a format <Wavelength>\t<absorbance@time0>\t<absorbance@time1>...
+        """
+        print(f'PhPhOxCalcShell().do_addexperiment(self, arg):') #LOG
+        arguments = self.parse_args(arg)
 
     def do_quit(self, arg):
         """
@@ -19,6 +36,31 @@ class PhPhOxCalcShell(cmd.Cmd):
         print('bye')
         return True
 
+    def parse_args(self, arg):
+        """
+        Parse args in the from 'key1=value1[ key2=value2]' and return corresponding dictionary
+
+        Parameters:
+        -----------
+            arg : str
+                arguments as key=value pairs separated by space
+
+        Returns:
+        --------
+            args_dict : dict
+                dictionary of arguments
+        """
+        print(f'PhPhOxCalcShell().parse_args(arg):') #LOG
+        args_dict = dict()
+        args = arg.split()
+        print(f'{args = }') #LOG
+        for a in args:
+            s = a.split('=')
+            args_dict[s[0]] = s[1]
+        print(f'{args_dict = }') #LOG
+        return args_dict
+
+
 if __name__ == '__main__':
-    print(f'{dir() = }')
+#    print(f'{dir() = }') #LOG
     PhPhOxCalcShell().cmdloop()

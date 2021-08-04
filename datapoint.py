65f91dea5d84fa7bb165fe62a1e7917498f42851
leybodv <leybodv@gmail.com>
2021-08-04 14:46:28 +0300

    new file:   datapoint.py: added class representing data point of phenol oxidation experiment

---
+++ datapoint.py
@@ -0,0 +1,11 @@
+# TODO: docs
+
+class DataPoint():
+    """
+    """
+
+    def __init__(self, time, wavelength, absorbance):
+        self.time = time
+        self.wavelength = wavelength
+        self.absorbance = absorbance
+        print(f'DataPoint().__init__(self, time, wavelength, absorbance):') #LOG

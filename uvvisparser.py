ba74877cda32a0432390d569c8d29a8104ed1e1a
leybodv <leybodv@gmail.com>
2021-08-09 19:20:43 +0300

    new file:   uvvisparser.py: added class for parsing uv-vis data

---
+++ uvvisparser.py
@@ -0,0 +1,14 @@
+# TODO: docs
+
+import numpy as np
+
+class UvVisParser():
+
+    def parse_calibration_from_file(self, file):
+        """
+        """
+        print(f'UvVisParser().parse_calibration_from_file(self, file):') #LOG
+        print(f'Processing {file}')
+        concentration = input(prompt='Enter concentration of solute [mg/L]: ')
+        wavelength, absorbance = np.loadtxt(fname = file, delimiter='\t', unpack=True, encoding='utf-8')
+        return (concentration, wavelength, absorbance)

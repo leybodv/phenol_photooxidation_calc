774d9a52a61f917fcfc4610de536e5cde3eb14d4
leybodv <leybodv@gmail.com>
2021-08-11 16:23:08 +0300

    modified:   calibrationpoint.py: commented out logging

--- calibrationpoint.py
+++ calibrationpoint.py
@@ -9,17 +9,17 @@ class CalibrationPoint():
         """
         print(f'CalibrationPoint().__init__(self, concentration, wavelength, absorbance):') #LOG
         self.concentration = concentration
-        self.wavelength = np.array(wavelength)
-        self.absorbance = np.array(absorbance)
+        self.wavelength = wavelength
+        self.absorbance = absorbance
 
     def get_absorbance_at(self, wavelength):
         """
         """
         print(f'CalibrationPoint().get_absorbance_at(self, wavelength):') #LOG
-        print(f'{self.concentration = }') #LOG
-        print(f'{self.absorbance = }') #LOG
-        print(f'{self.wavelength = }') #LOG
-        print(f'{self.wavelength == wavelength = }') #LOG
-        print(f'{type(wavelength) = }') #LOG
+#        print(f'{self.concentration = }') #LOG
+#        print(f'{self.absorbance = }') #LOG
+#        print(f'{self.wavelength = }') #LOG
+#        print(f'{self.wavelength == wavelength = }') #LOG
+#        print(f'{type(wavelength) = }') #LOG
         absorbance = self.absorbance[self.wavelength == wavelength][0]
         return absorbance

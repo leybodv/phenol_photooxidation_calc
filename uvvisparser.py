c4d9d6453f5c525df6d18757596ed8b6f5caa12c
leybodv <leybodv@gmail.com>
2021-08-11 15:09:21 +0300

    modified:   uvvisparser.py: input method takes no keyword arguments

--- uvvisparser.py
+++ uvvisparser.py
@@ -9,6 +9,6 @@ class UvVisParser():
         """
         print(f'UvVisParser().parse_calibration_from_file(self, file):') #LOG
         print(f'Processing {file}')
-        concentration = input(prompt='Enter concentration of solute [mg/L]: ')
+        concentration = input('Enter concentration of solute [mg/L]: ')
         wavelength, absorbance = np.loadtxt(fname = file, delimiter='\t', unpack=True, encoding='utf-8')
         return (concentration, wavelength, absorbance)

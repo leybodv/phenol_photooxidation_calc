e42ed44cdba35949464bc0706a061b8c4c902f8c
leybodv <leybodv@gmail.com>
2021-08-11 18:17:04 +0300

    modified:   plotter.py: added plot_calibration method

--- plotter.py
+++ plotter.py
@@ -49,3 +49,22 @@ class Plotter():
             ax.plot(point.wavelength, point.absorbance, label=point.concentration)
         ax.legend()
         plt.show(block=False)
+
+    def plot_calibration(self, calibration):
+        """
+        """
+        print(f'Plotter().plot_calibration(self, calibration):') #LOG
+        fig, (ax_uvvis, ax_calibration) = plt.subplots(ncols=2)
+        calibration_x = list()
+        calibration_y_data = list()
+        calibration_y_calc = list()
+        for point in calibration.calibration_points:
+            ax_uvvis.plot(point.wavelength, point.absorbance, label=point.concentration)
+            calibration_x.append(point.concentration)
+            calibration_y_data.append(point.get_absorbance_at(calibration.calibration_wavelength))
+            calibration_y_calc.append(calibration.linear_func(point.concentration, calibration.calibration_coefficient))
+        ax_uvvis.axvline(x=calibration.calibration_wavelength, color='red')
+        ax_calibration.scatter(calibration_x, calibration_y_data, c='red')
+        ax_calibration.plot(calibration_x, calibration_y_calc, c='blue')
+        ax_uvvis.legend()
+        plt.show(block=False)

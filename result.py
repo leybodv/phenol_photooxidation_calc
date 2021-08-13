dab2fe629b801aa4bc2d5d56e99c35c24f64c243
leybodv <leybodv@gmail.com>
2021-08-13 17:31:55 +0300

    new file:   result.py: created class Result which holds results of concentration calculation

---
+++ result.py
@@ -0,0 +1,21 @@
+# TODO: docs
+
+from experiment import Experiment
+
+class Result():
+
+    def __init__(self, experiment, calibrations):
+        """
+        """
+        print(f'Result().__init__(self, experiment, calibrations)') #LOG
+        self.sample_name = experiment.sample_name
+        self.result_points = self.calculate_results(experiment.data_points, calibrations)
+
+    def calculate_results(self, raw_data_points, calibrations):
+        """
+        """
+        print(f'Result().calculate_results(self, raw_data_points, calibrations)') #LOG
+        result_points = list()
+        for point in raw_data_points:
+            result_points.append(ResultPoint(point, calibrations)) # TODO: define class
+        return result_points

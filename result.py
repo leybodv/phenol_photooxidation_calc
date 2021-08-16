87fd2c197fd1a7b47ba4cc6fc903e266720fdc7a
leybodv <leybodv@gmail.com>
2021-08-16 17:31:49 +0300

    modified:   result.py: added parameters types in methods definitions

--- result.py
+++ result.py
@@ -4,14 +4,14 @@ from experiment import Experiment
 
 class Result():
 
-    def __init__(self, experiment, calibrations):
+    def __init__(self, experiment:Experiment, calibrations:list):
         """
         """
         print(f'Result().__init__(self, experiment, calibrations)') #LOG
         self.sample_name = experiment.sample_name
         self.result_points = self.calculate_results(experiment.data_points, calibrations)
 
-    def calculate_results(self, raw_data_points, calibrations):
+    def calculate_results(self, raw_data_points:list, calibrations:list):
         """
         """
         print(f'Result().calculate_results(self, raw_data_points, calibrations)') #LOG

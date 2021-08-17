aafb25e5805ed0865bc161a7cdec8fdc9b8fa664
leybodv <leybodv@gmail.com>
2021-08-17 11:45:56 +0300

    modified:   calibrationpoint.py: added getter methods

--- calibrationpoint.py
+++ calibrationpoint.py
@@ -1,6 +1,7 @@
 # TODO: docs
+# TODO: check access to calibraionpoint.wavelength and calibrationpoint.absorbance variables
 
-import numpy as np
+from spectrum import Spectrum
 
 class CalibrationPoint():
 
@@ -9,8 +10,7 @@ class CalibrationPoint():
         """
         print(f'CalibrationPoint().__init__(self, concentration, wavelength, absorbance):') #LOG
         self.concentration = concentration
-        self.wavelength = wavelength
-        self.absorbance = absorbance
+        self.spectrum = Spectrum(wavelength=wavelength, absorbance=absorbance)
 
     def get_absorbance_at(self, wavelength):
         """
@@ -23,3 +23,27 @@ class CalibrationPoint():
 #        print(f'{type(wavelength) = }') #LOG
         absorbance = self.absorbance[self.wavelength == wavelength][0]
         return absorbance
+
+    def get_wavelengths(self) -> array:
+        """
+        """
+        print(f'CalibrationPoint().get_wavelengths(self):') #LOG
+        return self.spectrum.get_wavelengths() # TODO: define method
+
+    def get_absorbances(self) -> array:
+        """
+        """
+        print(f'CalibrationPoint().get_absorbances(self):') #LOG
+        return self.spectrum.get_absorbances() # TODO: define method
+
+    def get_concentration(self) -> float:
+        """
+        """
+        print(f'CalibrationPoint().get_concentration(self):') #LOG
+        return self.concentration
+
+    def get_spectrum(self) -> Spectrum:
+        """
+        """
+        print(f'CalibrationPoint().get_spectrum(self)') #LOG
+        return self.spectrum

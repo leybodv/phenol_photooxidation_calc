1f7a96795dbae2af22b1578dfeba7db399186220
leybodv <leybodv@gmail.com>
2021-08-18 18:02:03 +0300

    modified:   datapoint.py: added getters

--- datapoint.py
+++ datapoint.py
@@ -7,7 +7,20 @@ class DataPoint():
     """
 
     def __init__(self, time, wavelength, absorbance):
+        """
+        """
+        print(f'DataPoint().__init__(self, time, wavelength, absorbance') #LOG
         self.time = time
         self.spectrum = Spectrum(wavelength=wavelength, absorbance=absorbance)
-        print(f'DataPoint().__init__(self, time, wavelength, absorbance):') #LOG
-        print(f'{self.time = }') #LOG
+
+    def get_wavelength(self):
+        """
+        """
+        print(f'DataPoint().get_wavelength(self):') #LOG
+        return self.spectrum.get_wavelength()
+
+    def get_absorbance(self):
+        """
+        """
+        print(f'DataPoint().get_absorbance(self):') #LOG
+        return self.spectrum.get_absorbance()

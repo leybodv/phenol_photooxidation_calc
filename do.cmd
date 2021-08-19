b39e329ecd4d0522510faac75f85d03f4d312268
leybodv <leybodv@gmail.com>
2021-08-19 12:25:22 +0300

    modified:   do.cmd: commented out come commands

--- do.cmd
+++ do.cmd
@@ -1,6 +1,6 @@
-addcalibration solute=benzoquinone solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/benzoquinone
-addcalibration solute=catechol solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/catechol
-addcalibration solute=formic-acid solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/formic-acid
+#addcalibration solute=benzoquinone solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/benzoquinone
+#addcalibration solute=catechol solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/catechol
+#addcalibration solute=formic-acid solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/formic-acid
 addcalibration solute=hydroquinone solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/hydroquinone
 addcalibration solute=phenol solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/phenol
 addexperiment sample_name=dl100 raw_data_path=/home/d/git/phenol_photooxidation_calc/test/dl100_uv-vis_phenol.dat

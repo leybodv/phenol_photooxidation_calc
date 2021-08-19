628d786f5c6de604eeb5e1892438e6fd49591f8b
leybodv <leybodv@gmail.com>
2021-08-19 17:52:11 +0300

    modified:   resultpoint.py: removed calibrations from class

--- do.cmd
+++ do.cmd
@@ -3,6 +3,6 @@ plotrawdata
 #addcalibration solute=benzoquinone solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/benzoquinone
 #addcalibration solute=catechol solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/catechol
 #addcalibration solute=formic-acid solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/formic-acid
-#addcalibration solute=hydroquinone solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/hydroquinone
+addcalibration solute=hydroquinone solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/hydroquinone
 addcalibration solute=phenol solvent=water folder=/home/d/git/phenol_photooxidation_calc/calibrations/phenol
 processexperiments verbose=True
--- resultpoint.py
+++ resultpoint.py
@@ -10,14 +10,15 @@ class ResultPoint():
 
     mask_points_from = 450
 
-    def __init__(self, experimental_point:DataPoint, calibrations:list):
+    def __init__(self, experimental_point:DataPoint, reference_spectra:list, reference_names:list, calibration_coefficients:np.ndarray, calibration_wavelengths:np.ndarray):
         """
         """
         print(f'ResultPoint().__init__(self, experimental_point, calibrations):') #LOG
         self.time = experimental_point.time
-        self.reference_spectra, self.reference_names = self.find_out_spectra(calibrations)
+        self.reference_spectra = reference_spectra
+        self.reference_names = reference_names
         self.coefficients = self.find_coefficients(experimental_point.spectrum, self.reference_spectra)
-        self.concentrations = self.find_concentrations(self.coefficients, self.reference_spectra, calibrations)
+        self.concentrations = self.find_concentrations(self.coefficients, self.reference_spectra, calibration_coefficients, calibration_wavelengths)
 
     def find_out_spectra(self, calibrations:list) -> tuple:
         """
@@ -38,13 +39,12 @@ class ResultPoint():
         """
         """
         print(f'ResultPoint().find_coefficients(self, spectrum, reference_spectra):') #LOG
-#        popt, pcov = spopt.curve_fit(lambda x, *params: self.get_fitted_y(x, reference_spectra, params), spectrum.get_wavelength(), spectrum.get_absorbance(), p0=np.full(len(reference_spectra), 0.5), bounds=(0, 1), sigma=1/spectrum.get_absorbance())
         truncated_spectrum = spectrum.truncate(self.mask_points_from, np.inf)
         truncated_reference_spectra = list()
         for reference_spectrum in reference_spectra:
             truncated_reference_spectra.append(reference_spectrum.truncate(self.mask_points_from, np.inf))
-        popt, pcov = spopt.curve_fit(lambda x, *params: self.get_fitted_y(x, truncated_reference_spectra, params), truncated_spectrum.get_wavelength(), truncated_spectrum.get_absorbance(), p0=np.full(len(reference_spectra), 0.5), bounds=(0, 1))
-#        popt, pcov = spopt.curve_fit(lambda x, *params: self.get_fitted_y(x, truncated_reference_spectra, params), truncated_spectrum.get_wavelength(), truncated_spectrum.get_absorbance(), p0=np.full(len(reference_spectra), 0.5), bounds=(0, 1), sigma=1/truncated_spectrum.get_absorbance())
+#        popt, pcov = spopt.curve_fit(lambda x, *params: self.get_fitted_y(x, truncated_reference_spectra, params), truncated_spectrum.get_wavelength(), truncated_spectrum.get_absorbance(), p0=np.full(len(reference_spectra), 0.5), bounds=(0, 1))
+        popt, pcov = spopt.curve_fit(lambda x, *params: self.get_fitted_y(x, truncated_reference_spectra, params), truncated_spectrum.get_wavelength(), truncated_spectrum.get_absorbance(), p0=np.full(len(reference_spectra), 0.5), bounds=(0, 1), sigma=1/truncated_spectrum.get_absorbance())
         print(f'{popt = }') #LOG
         return popt
 
@@ -58,15 +58,15 @@ class ResultPoint():
         y = np.sum(np.array(ys) * np.array(coefficients))
         return y
 
-    def find_concentrations(self, coefficients:np.ndarray, reference_spectra:list, calibrations:list) -> np.ndarray:
+    def find_concentrations(self, coefficients:np.ndarray, reference_spectra:list, calibration_coefficients:np.ndarray, calibration_wavelengths:np.ndarray) -> np.ndarray:
         """
         """
         print(f'ResultPoint().find_concentrations(self, coefficients, reference_spectra, calibrations):') #LOG
         concentrations = list()
-        for coefficient, spectrum, calibration in zip(coefficients, reference_spectra, calibrations):
+        for coefficient, spectrum, calibration_coefficient, calibration_wavelength in zip(coefficients, reference_spectra, calibration_coefficients, calibration_wavelengths):
             corrected_spectrum = Spectrum(wavelength=spectrum.get_wavelength(), absorbance=spectrum.get_absorbance()*coefficient)
-            calibration_absorbance = corrected_spectrum.get_absorbance_at(calibration.calibration_wavelength)
-            concentrations.append(calibration_absorbance / calibration.calibration_coefficient)
+            calibration_absorbance = corrected_spectrum.get_absorbance_at(calibration_wavelength)
+            concentrations.append(calibration_absorbance / calibration_coefficient)
         return np.array(concentrations)
 
     def get_reference_names(self) -> list:

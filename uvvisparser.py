02947d7e21084661f173c15cab0df949808f6567
leybodv <leybodv@gmail.com>
2021-08-20 22:26:32 +0300

    odified:   uvvisparser.py: added comments

--- uvvisparser.py
+++ uvvisparser.py
@@ -1,13 +1,28 @@
-# TODO: docs
-
 import numpy as np
 
 class UvVisParser():
+    """
+    utility class for parsing uv-vis data
+    """
 
     def parse_calibration_from_file(self, file):
         """
+        ask user for concentration used for calibration and parse corresponding uv-vis spectrum
+
+        parameters
+        ----------
+        file : str or Path
+            file with calibration spectrum data
+
+        returns
+        -------
+        concentration : float
+            concentration used for calibration
+        wavelength : numpy.ndarray[float]
+            array of wavelengths parsed from file
+        absorbance : numpy.ndarray[float]
+            array of absorbances parsed from file
         """
-        print(f'UvVisParser().parse_calibration_from_file(self, file):') #LOG
         concentration = input(f'Importing file {file}. Enter concentration of solute [mmol/L]: ')
         concentration = float(concentration)
         wavelength, absorbance = np.loadtxt(fname = file, delimiter='\t', unpack=True, encoding='utf-8', skiprows=1)

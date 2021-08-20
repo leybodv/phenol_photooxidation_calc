ff0f16885e89e6a32e2ac99fa6b931dc5ad3c03e
leybodv <leybodv@gmail.com>
2021-08-20 18:03:26 +0300

    modified:   datapoint.py: removed logging; added comments

--- datapoint.py
+++ datapoint.py
@@ -1,27 +1,52 @@
-# TODO: docs
-
 from spectrum import Spectrum
 import numpy as np
 
 class DataPoint():
     """
+    class represents experimental data point with information about time and uv-vis spectrum
+
+    instance variables
+    ------------------
+    time : float
+        time from the beginning of experiment at which specimen was collected for uv-vis characterization
+    spectrum : Spectrum
+        experimental uv-vis spectrum of sample
     """
 
     def __init__(self, time, wavelength, absorbance):
         """
+        assigns parameters to instance variables
+
+        parameters
+        ----------
+        time : float
+            time from the beginning of experiment at which specimen was collected for uv-vis characterization
+        wavelength : numpy.ndarray[float] or list[float]
+            array of wavelengths of uv-vis spectrum
+        absorbance : numpy.ndarray[float] or list[float]
+            array of absorbances of uv-vis spectrum
         """
-        print(f'DataPoint().__init__(self, time, wavelength, absorbance') #LOG
         self.time = time
         self.spectrum = Spectrum(wavelength=wavelength, absorbance=absorbance)
 
     def get_wavelength(self) -> np.ndarray:
         """
+        getter to obtain array of wavelengths of uv-vis spectrum
+
+        returns
+        -------
+        wavelengths : numpy.ndarray[float]
+            array of wavelengths of uv-vis spectrum
         """
-        print(f'DataPoint().get_wavelength(self):') #LOG
         return self.spectrum.get_wavelength()
 
     def get_absorbance(self) -> np.ndarray:
         """
+        getter to obtain array of absorbances of uv-vis spectrum
+
+        returns
+        -------
+        absorbances : numpy.ndarray[float]
+            array of absorbances of uv-vis spectrum
         """
-        print(f'DataPoint().get_absorbance(self):') #LOG
         return self.spectrum.get_absorbance()

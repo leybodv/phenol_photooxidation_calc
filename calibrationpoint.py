48794eb5cecf5aeae21e4005b729ec6e190054cd
leybodv <leybodv@gmail.com>
2021-08-20 17:16:23 +0300

    modified:   calibrationpoint.py: added comments

--- calibrationpoint.py
+++ calibrationpoint.py
@@ -1,44 +1,91 @@
-# TODO: docs
-
 from spectrum import Spectrum
 import numpy as np
 
 class CalibrationPoint():
+    """
+    class represents single calibration point which includes information about concentration, used for calibration and uv-vis spectrum obtained
+
+    instance variables
+    ------------------
+    concentration : float
+        concentration of solute used for calibration
+    spectrum : Spectrum
+        uv-vis spectrum of this calibration point
+    """
 
     def __init__(self, concentration, wavelength, absorbance):
         """
+        assigns parameters to instance variables
+
+        parameters
+        ----------
+        concentration : float
+            concentration of solute used for calibration
+        wavelength : numpy.ndarray[float] or list[float]
+            array of wavelengths of calibration uv-vis spectrum
+        absorbance : numpy.ndarrat[float] or list[float]
+            array of absorbances of calibration uv-vis spectrum
         """
-        print(f'CalibrationPoint().__init__(self, concentration, wavelength, absorbance):') #LOG
         self.concentration = concentration
         self.spectrum = Spectrum(wavelength=wavelength, absorbance=absorbance)
 
     def get_absorbance_at(self, wavelength):
         """
+        returns value of absorbance at specified wavelength. Invokes Spectrum.get_absorbance_at(wavelength) method
+
+        parameters
+        ----------
+        wavelength : float
+            wavelength to get absorbance value at
+
+        returns
+        -------
+        absorbance : float
+            absorbance at specified wavelength
         """
-        print(f'CalibrationPoint().get_absorbance_at(self, wavelength):') #LOG
         absorbance = self.spectrum.get_absorbance_at(wavelength)
         return absorbance
 
     def get_wavelength(self) -> np.ndarray:
         """
+        getter to obtain array of wavelengths of calibration spectrum
+
+        returns
+        -------
+        wavelengths : numpy.ndarray
+            wavelengths of calibration spectrum
         """
-        print(f'CalibrationPoint().get_wavelength(self):') #LOG
-        return self.spectrum.get_wavelength() # TODO: define method
+        return self.spectrum.get_wavelength()
 
     def get_absorbance(self) -> np.ndarray:
         """
+        getter to obtain array of absorbances of calibration spectrum
+
+        returns
+        -------
+        absorbances : numpy.ndarray
+            absorbances of calibration spectrum
         """
-        print(f'CalibrationPoint().get_absorbance(self):') #LOG
         return self.spectrum.get_absorbance()
 
     def get_concentration(self) -> float:
         """
+        getter to obtain concentration used for calibration
+
+        returns
+        -------
+        concentration : float
+            concentration used for calibration
         """
-        print(f'CalibrationPoint().get_concentration(self):') #LOG
         return self.concentration
 
     def get_spectrum(self) -> Spectrum:
         """
+        getter to get calibration spectrum
+
+        returns
+        -------
+        spectrum : Spectrum
+            calibration spectrum
         """
-        print(f'CalibrationPoint().get_spectrum(self)') #LOG
         return self.spectrum

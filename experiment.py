1a0b820629348cfce445dd9eed46af235d0012cc
leybodv <leybodv@gmail.com>
2021-09-02 13:18:46 +0300

    added initial concentration of phenol and peroxide to method definitions

--- experiment.py
+++ experiment.py
@@ -14,9 +14,13 @@ class Experiment():
         path to file with uv-vis spectra
     data_points : list[DataPoint]
         collection of data points with uv-vis spectra and times
+    phenol_init_conc : float
+        initial concentration of phenol in μM
+    peroxide_init_conc : float
+        initial concentration of peroxide in μM
     """
 
-    def __init__(self, sample_name, raw_data_path, data_format:str):
+    def __init__(self, sample_name, raw_data_path, data_format:str, phenol_init_conc:float, peroxide_init_conc:float):
         """
         assigns parameters to instance variables and parses experimental data
 
@@ -28,9 +32,15 @@ class Experiment():
             path to file or folder with experimental data
         data_format : str
             format of experimental data. If 'file', data must be in a single file; if 'folder', data must be in separate files in a folder
+        phenol_init_conc : float
+            initial concentration of phenol in μM
+        peroxide_init_conc : float
+            initial concentration of peroxide in μM
         """
         self.sample_name = sample_name
         self.raw_data_path = raw_data_path
+        self.phenol_init_conc = phenol_init_conc
+        self.peroxide_init_conc = peroxide_init_conc
         if data_format == 'file':
             self.data_points = self.parse_uv_vis(raw_data_path)
         elif data_format == 'folder':
@@ -63,3 +73,25 @@ class Experiment():
                     absorbance = data[i] + abs(data[i].min())
                 data_points.append(DataPoint(time=header[i], wavelength=data[0], absorbance=absorbance))
         return data_points
+
+    def get_phenol_init_concentration(self) -> float:
+        """
+        getter to obtain initial concentration of phenol in μM
+
+        returns
+        -------
+        phenol_init_conc : float
+            initial concentration of phenol in μM
+        """
+        return self.phenol_init_conc
+
+    def get_peroxide_init_concentration(self) -> float:
+        """
+        getter to obtain initial concentration of peroxide in μM
+
+        returns
+        -------
+        peroxide_init_conc : float
+            initial concentration of peroxide in μM
+        """
+        return self.peroxide_init_conc

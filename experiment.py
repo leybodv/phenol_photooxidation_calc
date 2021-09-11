4a3239eadb61bfa117c95a1dfd307bddf7f6dba1
leybodv <leybodv@gmail.com>
2021-09-11 22:47:23 +0300

    removed parse_uv_vis method

--- experiment.py
+++ experiment.py
@@ -42,38 +42,12 @@ class Experiment():
         self.phenol_init_conc = float(phenol_init_conc)
         self.peroxide_init_conc = float(peroxide_init_conc)
         if data_format == 'file':
-            self.data_points = self.parse_uv_vis(raw_data_path)
+            self.data_points = UvVisParser().parse_experimental_from_file(raw_data_path)
         elif data_format == 'folder':
             self.data_points = UvVisParser().parse_experimental_from_folder(raw_data_path)
         else:
             print(f'Cannot recognize experimental data of format {data_format}')
             return
-
-    def parse_uv_vis(self, raw_data_path):
-        """
-        parses uv-vis data. file must be in a format <wavelength><tab><absorbance@time0><tab><absorbance@time1>... file also must contain single row header in a format <some-text><tab><time0><tab><time1>...
-
-        parameters
-        ----------
-        raw_data_path : str or Path
-            path to file with experimental data
-
-        returns
-        -------
-        data_points : list[DataPoint]
-            experimental data points with time and spectrum information
-        """
-        header = np.loadtxt(fname=raw_data_path, delimiter='\t', dtype='str', encoding='utf-8', max_rows=1, unpack=True)
-        data = np.loadtxt(fname=raw_data_path, delimiter='\t', skiprows=1, unpack=True, encoding='utf-8')
-        data_points = list()
-        for i in range(len(data)):
-            if i != 0:
-                absorbance = data[i]
-                if np.any(data[i] < 0):
-                    absorbance = data[i] + abs(data[i].min())
-                data_points.append(DataPoint(time=header[i], wavelength=data[0], absorbance=absorbance))
-        data_points.sort(key=lambda point: point.get_time())
-        return data_points
 
     def get_phenol_init_concentration(self) -> float:
         """

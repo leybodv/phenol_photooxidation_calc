76655857cf83f39ded67234d437dbc327652e78d
leybodv <leybodv@gmail.com>
2021-09-11 22:31:54 +0300

    modified:   do.cmd

--- do.cmd
+++ do.cmd
@@ -1,6 +1,5 @@
 #initial concentration of phenol 200 and peroxide 48706 μM, increased to take into account possible error
 addexperiment id=dl100 path=/home/d/yandex-disk/science/6-defBN-phenol-photooxidation/photocatalysis/dl100-1 format=folder phenol=210 peroxide=48710
-plotrawdata
 addcalibration solute=benzoquinone solvent=water path=/home/d/git/phenol_photooxidation_calc/calibrations/benzoquinone
 addcalibration solute=catechol solvent=water path=/home/d/git/phenol_photooxidation_calc/calibrations/catechol
 addcalibration solute=formic-acid solvent=water path=/home/d/git/phenol_photooxidation_calc/calibrations/formic-acid

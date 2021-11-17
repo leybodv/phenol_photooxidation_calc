32c2e600f4e8bfe8f2723423c3f709815103533c
leybodv <leybodv@gmail.com>
2021-11-17 16:02:02 +0300

    at 0th time moment peroxide concentration is not more than its initial concentration

--- resultpoint.py
+++ resultpoint.py
@@ -100,7 +100,7 @@ class ResultPoint():
                     if peroxide_init_conc == 0:
                         bh = 10 ** (-9)
                     else:
-                        bh = np.inf
+                        bh = peroxide_init_conc * conc_calibration_coef / reference_spectrum.get_absorbance_at(calibration_wavelength)
                 elif ref_compound in ['benzoquinone', 'catechol', 'hydroquinone', 'formic-acid']:
                     bh = 10 ** (-9)
                 else:
